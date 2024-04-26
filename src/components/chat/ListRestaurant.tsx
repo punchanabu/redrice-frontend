309def0995f6c07728d68ccdea4dbf0dddb6c8ee
KinkPapawin <kinkcr7@gmail.com>
2024-04-26 20:58:04 +0700

    chat for restaurant login to chatwith user

--- src/components/chat/ListRestaurant.tsx
+++ src/components/chat/ListRestaurant.tsx
@@ -12,6 +12,7 @@ import { sessionRoom } from "@/types/chat";
 import { useSession } from "next-auth/react"
 import { getOneRestaurant } from "@/lib/restaurant"
 import Restaurant from "@/types/restaurant"
+import { getUserById } from "@/lib/auth"
 
 const mockdata:Array<{roomid:string,name:string,img:string,msg:string,time:string}>=[{roomid:'1',name:'Pizza Hutz  1150',img:'',msg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, laboriosam.',time:'10:30 AM'},{roomid:'2',name:'Momo',img:'',msg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, laboriosam.',time:'10:30 AM'},{roomid:'3',name:'7-11',img:'',msg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, laboriosam.',time:'10:30 AM'},{roomid:'4',name:'Tenya',img:'',msg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, laboriosam.',time:'10:30 AM'},{roomid:'',name:'Yayoi',img:'',msg:'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae, laboriosam.',time:'10:30 AM'}]
 
@@ -36,45 +37,99 @@ export default function ListRestaurant({ setroomid,data, handleJoin, socket, set
 
 
     //Fetch restaurant from getOneRestaurant for using Restaurant Schema
-    useEffect(() => {
-        const fetchRestaurantDetails = async () => {
-          if (token && filterrestaurants.length > 0) {
-            const promises = filterrestaurants.map(async (restaurant: any) => {
-              try {
-                const { restaurantId } = restaurant;
-                const fetchedRestaurant = await getOneRestaurant(restaurantId.toString(), token);
-                console.log(fetchedRestaurant)
+    // useEffect(() => {
+    //     const fetchRestaurantDetails = async () => {
+    //       if (token && filterrestaurants.length > 0) {
+    //         const promises = filterrestaurants.map(async (restaurant: any) => {
+    //           try {
+    //             const { restaurantId } = restaurant;
+    //             const fetchedRestaurant = await getOneRestaurant(restaurantId.toString(), token);
+    //             console.log(fetchedRestaurant)
 
-                return { ...restaurant, details: fetchedRestaurant };
+    //             return { ...restaurant, details: fetchedRestaurant };
+    //           } catch (error) {
+    //             console.error(`Error fetching restaurant details for userId ${restaurant.userId}:`, error);
+    //             return { ...restaurant, details: null };
+    //           }
+    //         });
+    
+    //         const restaurantsWithDetails = await Promise.all(promises);
+    //         setRestaurantsDetails(restaurantsWithDetails);
+    //       }
+    //     };
+    
+    //     fetchRestaurantDetails();
+    //   }, [token, filterrestaurants]);
+
+
+      
+        //   useEffect(() => {
+        //     const fetchRestaurantDetails = async () => {
+        //       if (token && filterrestaurants.length > 0) {
+        //         const promises = filterrestaurants.map(async (restaurant: any) => {
+        //           try {
+        //             const { restaurantId } = restaurant;
+        //             const fetchedRestaurant = await getOneRestaurant(restaurantId.toString(), token);
+        //             console.log(fetchedRestaurant)
+
+        //             return { ...restaurant, details: fetchedRestaurant };
+        //           } catch (error) {
+        //             console.error(`Error fetching restaurant details for userId ${restaurant.userId}:`, error);
+        //             return { ...restaurant, details: null };
+        //           }
+        //         });
+        
+        //         const restaurantsWithDetails = await Promise.all(promises);
+        //         setRestaurantsDetails(restaurantsWithDetails);
+        //       }
+        //     };
+        
+        //     fetchRestaurantDetails();
+        //   }, [token, filterrestaurants]);
+
+        const [filterUsers, setFilterUsers] = useState<any[]>([]);
+        const [usersDetails, setUsersDetails] = useState<any[]>([]);
+
+      useEffect(() => {
+        const fetchUserDetails = async () => {
+            console.log(token)
+          if (token) {
+            const promises = data.map(async (user: any) => {
+              try {
+                const { userId } = user;
+                console.log(userId)
+                const fetchedUser = await getUserById( token , userId); // Assuming getOneRestaurant fetches user details
+                console.log(fetchedUser);
+    
+                return { ...user, details: fetchedUser };
               } catch (error) {
-                console.error(`Error fetching restaurant details for userId ${restaurant.userId}:`, error);
-                return { ...restaurant, details: null };
+                console.error(`Error fetching user details for userId ${user.userId}:`, error);
+                return { ...user, details: null };
               }
             });
     
-            const restaurantsWithDetails = await Promise.all(promises);
-            setRestaurantsDetails(restaurantsWithDetails);
+            const usersWithDetails = await Promise.all(promises);
+            setUsersDetails(usersWithDetails);
           }
         };
     
-        fetchRestaurantDetails();
-      }, [token, filterrestaurants]);
-      
+          fetchUserDetails();
+      }, [token, filterUsers, session]);
 
       const handleRoomClick = (sessionId: string) => {
         setroomid(sessionId);
         handleJoin(sessionId, socket);
         setMessageList([]);
       };
-      restaurantsDetails.map((restaurant: any) =>{
-        console.log(restaurant.details.name)
-      })
+    //   restaurantsDetails.map((restaurant: any) =>{
+    //     console.log(restaurant.details.name)
+    //   })
         
     return (
         <div className="w-full h-full flex flex-col gap-3">
           <div className="px-3 flex gap-1 sm:max-xl:gap-0">
             <div className="w-[88%]">
-              <Searchbox data={data as DataFilter[]} filter={setFilterRestaurants}></Searchbox>
+              <Searchbox data={data as DataFilter[]} filter={setFilterRestaurants || setFilterUsers}></Searchbox>
             </div>
             <div className="p-0.5 text-center flex items-center relative">
               <BiMessageSquare className="text-gray-300 hover:text-gray-400 text-3xl" />
@@ -87,7 +142,7 @@ export default function ListRestaurant({ setroomid,data, handleJoin, socket, set
           <div className="w-full overflow-x-auto">
             {restaurantsDetails.map((restaurant: any) => (
               <div
-                key={restaurant.sessionId}
+                key={restaurant?.sessionId} 
                 className="hover:bg-slate-100 p-3 flex gap-2 cursor-pointer"
                 onClick={() => handleRoomClick(restaurant.sessionId)}
               >
@@ -96,21 +151,50 @@ export default function ListRestaurant({ setroomid,data, handleJoin, socket, set
                     alt="Restaurant Image"
                     width={60}
                     height={60}
-                    src={restaurant.details.imageUrl}
+                    src={restaurant?.details?.imageUrl || "/img/user/user1.png"}
                     className="h-full object-contain rounded-full flex items-center"
                   />
                 </div>
                 <div className="w-2/3 flex flex-col">
-                  <h1 className="w-full line-clamp-1 text-redrice-yellow">{restaurant.details.name}</h1>
+                  <h1 className="w-full line-clamp-1 text-redrice-yellow">{restaurant?.details?.name || "user"}</h1>
                   <h1 className="w-full line-clamp-1 text-slate-300 bold">
                     Start a Conversation
                   </h1>
                 </div>
                 <div className="w-1/6 flex flex-col items-start">
-                  <h1 className="text-slate-300 bold">{restaurant.details.time}</h1>
+                  <h1 className="text-slate-300 bold">{restaurant?.details?.time }</h1>
                 </div>
               </div>
-            ))}
+            ))} 
+            : 
+            {
+            usersDetails.map((user: any) => (
+                <div
+                key={user.sessionId} 
+                className="hover:bg-slate-100 p-3 flex gap-2 cursor-pointer"
+                onClick={() => handleRoomClick(user.sessionId)}
+                >
+                <div className="w-1/6 flex items-center">
+                    <Image
+                    alt="Restaurant Image"
+                    width={60}
+                    height={60}
+                    src={user?.details?.imageUrl || "/img/user/user1.png"}
+                    className="h-full object-contain rounded-full flex items-center"
+                    />
+                </div>
+                <div className="w-2/3 flex flex-col">
+                    <h1 className="w-full line-clamp-1 text-redrice-yellow">{user?.details?.name || "user"}</h1>
+                    <h1 className="w-full line-clamp-1 text-slate-300 bold">
+                    Start a Conversation
+                    </h1>
+                </div>
+                <div className="w-1/6 flex flex-col items-start">
+                    <h1 className="text-slate-300 bold">{user?.details?.time }</h1>
+                </div>
+                </div>
+            ))               
+            }
           </div>
         </div>
       );
